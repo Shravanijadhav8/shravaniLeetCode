7ead272a7ceb40836602c5f954b5cf0a434d164c
Shravanijadhav8 <70316395+Shravanijadhav8@users.noreply.github.com>
2022-09-24 23:53:06 -0400

    Time: 212 ms (5.27%), Space: 17.6 MB (7.75%) - LeetHub

---
+++ 252-meeting-rooms/252-meeting-rooms.py
@@ -0,0 +1,24 @@
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        intervals.sort(key= lambda x:x[0])
+        
+        if intervals == [] or len(intervals) == 1:
+            return True
+        
+        lowerBound, upperBound = intervals.pop(0)
+        i=0
+        while i < len(intervals):
+            left, right = intervals[i][0], intervals[i][1]
+            if (right >= lowerBound) and  (left < upperBound):
+                return False
+           
+            else:
+                intervals.insert(i, [lowerBound, upperBound])
+                i += 1
+                lowerBound, upperBound = intervals[i][0], intervals[i][1]
+                i+=1
+                #left, right = intervals[i][0], intervals[i][1]
+        return True
+        
+        
+            
