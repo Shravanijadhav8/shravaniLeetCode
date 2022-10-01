bf72b599f1fb0143461650271e9f753e1a2d1a99
Shravanijadhav8 <70316395+Shravanijadhav8@users.noreply.github.com>
2022-10-01 12:09:20 -0400

    Time: 886 ms (5.07%), Space: 15.8 MB (34.13%) - LeetHub

---
+++ 169-majority-element/169-majority-element.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        hashmap = defaultdict(int)
+        
+        for i in nums:
+            #if i not in hashmap:
+            hashmap[i] += 1
+            print(hashmap)
+        
+        for key in hashmap:
+            if hashmap[key] >= len(nums)/2:
+                return key
+            else:
+                i += 1
+                
