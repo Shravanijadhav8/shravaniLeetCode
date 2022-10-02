94fb8242fd273f56e686b90181c2e5b50132be34
Shravanijadhav8 <70316395+Shravanijadhav8@users.noreply.github.com>
2022-10-01 22:55:33 -0400

    Time: 76 ms (9.17%), Space: 14 MB (22.45%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.py
+++ 409-longest-palindrome/409-longest-palindrome.py
@@ -1,32 +1,34 @@
 from collections import defaultdict
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-#         pali = defaultdict(int)
-        
-#         for i in s:
-#             if i not in pali:
+        pali = defaultdict(int)
+        for i in s:
+            pali[i] += 1
+ #            if i not in pali:
 #                 pali[i] = 1
 #             else:
-#                 pali[i] += 1
+            
 #         print(pali)
-        counter = defaultdict(int)
-        for letter in s:
-            counter[letter] += 1
-        
-        countEven = 0
-        countOne = 0
+        result = 0
+        #count = Counter(s)
+        countOnes = 0
         countOdd = 0
-        for key, val in counter.items():
-            if val % 2 == 0:
-                countEven += val
-            elif val == 1:
-                countOne = 1
-            else:
-                countOdd = countOdd + (val - 1)
-        if countOne == 0 and countOdd > 0:
-            countOne = 1
         
-        return countOne + countEven + countOdd
+        
+        for i, val in pali.items():
+            if val == 1:
+                countOnes = 1
+                
+            elif val % 2 == 0:
+                result += val
+            else:
+                countOdd = countOdd + (val-1)
+        if countOnes == 0 and countOdd > 0:
+            countOnes =1
+                    
+        
+                
+        return result+countOnes+countOdd
             
             
                 
