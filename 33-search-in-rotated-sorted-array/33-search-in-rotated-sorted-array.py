9ab6ea7783a528a5100b67a53e6d97eee6706644
Shravanijadhav8 <70316395+Shravanijadhav8@users.noreply.github.com>
2022-10-08 19:51:28 -0400

    Time: 50 ms (83.82%), Space: 14.2 MB (55.36%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,22 +1,27 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        def binarySearch(nums, low, high, target):  
-            if high >= low:
-                mid = (low+high)//2
-
-                if nums[mid] == target:
-                    #result = mid
-                    return mid
-                elif nums[mid] > target >= nums[low]:
-                    return binarySearch(nums, low, mid-1, target)
-                elif nums[mid] < target <= nums[high]:
-                    return binarySearch(nums, mid+1, high, target)
-                elif nums[mid] > nums[high]:
-                    return binarySearch(nums, mid+1, high, target)
-                #elif nums[mid] < nums[low]:
-                else:
-                    return binarySearch(nums, low, mid-1, target)
-            else: return -1
-                
         
-        return binarySearch(nums, 0, len(nums)-1, target)
+            low, high = 0, len(nums) -1
+            mid = 0
+
+            while low <= high:
+                mid = (low+high)//2
+                
+                if nums[mid] == target:
+                    return mid
+                
+                if nums[low] > nums[mid]:
+                    if nums[mid] <= target <= nums[high]:
+                        low = mid + 1
+                    else:
+                        high = mid -1
+                    
+                else:
+                    if nums[low] <= target <= nums[mid]:
+                        high = mid - 1
+                    else:
+                        low = mid + 1
+                    
+            return -1
+                
+                
