fc798cded0b0bd5f624f00af09a9e51a713992ca
Shravanijadhav8 <70316395+Shravanijadhav8@users.noreply.github.com>
2022-10-19 14:23:11 -0400

    Time: 66 ms (73.16%), Space: 17.1 MB (13.94%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -6,17 +6,35 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        nodes = []
-        
         def dfs(root):
-            if not root:
-                return
-            dfs(root.left)
-            nodes.append(root.val)
-            dfs(root.right)
-        dfs(root)
-        
-        for i in range(len(nodes)-1):
-            if nodes[i+1] <= nodes[i]:
+            nonlocal prev
+            if not root: return True
+            if not dfs(root.left): return False
+            if root.val <= prev:
                 return False
-        return True
+            prev = root.val
+            return dfs(root.right)
+            
+            
+        prev = float('-inf')
+        return dfs(root)
+        
+        
+        
+        
+        
+        
+#         nodes = []
+        
+#         def dfs(root):
+#             if not root:
+#                 return
+#             dfs(root.left)
+#             nodes.append(root.val)
+#             dfs(root.right)
+#         dfs(root)
+        
+#         for i in range(len(nodes)-1):
+#             if nodes[i+1] <= nodes[i]:
+#                 return False
+#         return True
