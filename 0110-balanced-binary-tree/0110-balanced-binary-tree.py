1ba5914898c6a10b48222a661319aa0e23147268
Shravanijadhav8 <70316395+Shravanijadhav8@users.noreply.github.com>
2022-11-12 19:14:53 -0500

    Time: 118 ms (34.06%), Space: 18.9 MB (29.49%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        unBalanced = False
+        def height(root,level):
+            nonlocal unBalanced
+            if not root: return level
+        
+            left = height(root.left,level + 1)
+            right = height(root.right,level + 1)
+            #print(f"root: {root.val} left:{left} right:{right}")
+            if abs(left - right) > 1: unBalanced = True 
+            return max(left,right) 
+            
+            
+        height(root,0)
+        if unBalanced: 
+            return False 
+        else: 
+            return True
+            
+            
+            
