85aae36a64591838724aa6820c437d744acf839e
Shravanijadhav8 <70316395+Shravanijadhav8@users.noreply.github.com>
2022-11-14 18:32:04 -0500

    Time: 101 ms (59.96%), Space: 18.9 MB (29.64%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -6,6 +6,34 @@
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        flag = False
+        def dfsHeight(root, level):
+            nonlocal flag
+            if not root: return level
+            left = dfsHeight(root.left, level+1)
+            right = dfsHeight(root.right, level+1)
+            if abs(left-right) > 1: flag = True
+            return max(left, right)
+        
+        dfsHeight(root, 0)
+        return False if flag else True
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        """
         unBalanced = False
         def height(root,level):
             nonlocal unBalanced
@@ -13,13 +41,12 @@ class Solution:
         
             left = height(root.left,level + 1)
             right = height(root.right,level + 1)
-            #print(f"root: {root.val} left:{left} right:{right}")
             if abs(left - right) > 1: unBalanced = True 
             return max(left,right) 
             
             
         height(root,0)
         return False if unBalanced else True
-            
+          """  
             
             
